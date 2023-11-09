c2a683aa64358efa3e2896c6d1c15568222216d2
phafezi <phafezi@ucsb.edu>
2023-11-09 13:44:45 -0800

    added pytests

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 
-import os
 
+import logging
+from fastapi import Request, status
+from fastapi.exceptions import RequestValidationError
+
+app = FastAPI()
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+	exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+	logging.error(f"{request}: {exc_str}")
+	content = {'status_code': 10422, 'message': exc_str, 'data': None}
+	return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+import os, sys
+
+sys.path.append("/Users/parsahafezi/Workspace/DermaQ/Skin-Oracle/backend")
 # make a directory to store images
 working_dir = os.getcwd()
 image_dir = f"{working_dir}/images"
@@ -29,7 +44,9 @@ async def create_upload_file(uploaded_file: UploadFile = File(...)):
     with open(file_location, "wb+") as file_object:
         file_object.write(uploaded_file.file.read())
 
+    print(uploaded_file.filename)
     #todo pass the location of the file to image processing pipeline and get valid prediction
     response_content = {"info": f"file '{uploaded_file.filename}' saved at '{file_location}'", "prediction": -1}
     
-    return JSONResponse(content=response_content)
+    return response_content
+    #return JSONResponse(content=response_content)
--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -20,20 +20,28 @@ def test_read_main():
     assert response.json() == {"message": "Hello World"}
 
 def test_valid_image():
-    file_location = f"{working_dir}/test_images/ucsb_test.png"
-    print(file_location)
-    with open(file_location, "rb") as image_file:
-        response = client.post("/predict", files={'file': image_file})
+    
+    image_file = open("tests/test_images/ucsb_test.png", 'rb')
 
-    # assert response.status_code == 200
-    response_content = {"info": f"file 'ucsb_test.png' saved at '{file_location}'", "prediction": -1}
-    assert response.json() == JSONResponse(content=response_content)
+    response = client.post("/predict", files={"uploaded_file": ("ucsb_test.png", image_file, "image/jpeg")})
+    print(response.json())
+    image_file.close() 
+
+    response_content = {"info": "file 'ucsb_test.png' saved at '/Users/parsahafezi/Workspace/DermaQ/Skin-Oracle/backend/images/ucsb_test.png'", "prediction": -1}
+    assert response.status_code == 200
+    assert response.json() == response_content
+
+
+
+
 
 def test_invalid_image():
-    file_location = f"{working_dir}/test_images/ucsb_test.heic"
-    with open(file_location, "rb") as image_file:
-        response = client.post("/predict", files={'file': ('ucsb_test.heic', image_file)})
+    image_file = open("tests/test_images/ucsb_test.heic", 'rb')
 
-    response_content = {"Invalid photo format"}    
-    assert response.json() == JSONResponse(content=response_content)
+    response = client.post("/predict", files={"uploaded_file": ("ucsb_test.heic", image_file, "image/jpeg")})
+    print(response.json())
+    image_file.close() 
+
+    response_content = {"detail": "Invalid photo format"}      
+    assert response.json() == response_content
     assert response.status_code == 415
